85309b582fe843ac6a80edb01613a5ba699ddfb4
mansigaikwad <mansigaikwad14@gmail.com>
2025-02-21 11:38:42 +0530

    changes to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Tectonic</h1>
+      <h1>Tectonic Software Services</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
